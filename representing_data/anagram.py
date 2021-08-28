ff350af302a7074155a28fa7345300d0bad4b242
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-08-28 20:54:58 +0200

    second solution using lookup dict

--- representing_data/anagram.py
+++ representing_data/anagram.py
@@ -5,10 +5,17 @@ def turn_a_word_into_a_list(word):
     letters = [char for char in word.lower()]
     return letters
 
+
 def check_both_words_same_letters(word1, word2):
     cond1 = len(word1) == len(word2)
     cond2 = all(item in word1 for item in word2)
     return cond1 and cond2
+
+
+def turn_into_sorted_string(word):
+    list_of_word_char = turn_a_word_into_a_list(word)
+    sorted_str = "".join(sorted(list_of_word_char))
+    return sorted_str
 
 
 def find_anagrams(letters, words):
@@ -18,14 +25,23 @@ def find_anagrams(letters, words):
     :param words: A set of lowercase, alphabetic English words in a word bank.
     :return: A set of anagrams of the given letters found in the word bank.
     """
-    list_of_letters = turn_a_word_into_a_list(letters)
-    output_set = set()
-    for word in words:
-        list_of_word_char = turn_a_word_into_a_list(word)
-        if check_both_words_same_letters(list_of_letters, list_of_word_char):
-            output_set.add(word)
+    ## 1st Solution
+    # list_of_letters = turn_a_word_into_a_list(letters)
+    # output_set = set()
+    # for word in words:
+    #     list_of_word_char = turn_a_word_into_a_list(word)
+    #     if check_both_words_same_letters(list_of_letters, list_of_word_char):
+    #         output_set.add(word)
 
-    return output_set
+    # 2nd solution
+    lookup = {}
+    for word in words:
+        key_to_lookup = turn_into_sorted_string(word)
+        if key_to_lookup not in lookup:
+            lookup[key_to_lookup] = set()
+        lookup[key_to_lookup].add(word)
+    check_this_word = turn_into_sorted_string(letters)
+    return lookup.get(check_this_word, set())
 
 
 
@@ -33,5 +49,3 @@ if __name__ == '__main__':
     while True:
         letters = input("What letters would you like to find the anagram of? ").lower().strip()
         print(find_anagrams(letters, english_words_small))
-    #  for anagram in find_anagrams(letters, english_words_small):
-    #     print(anagram)
