fccead714225a836a98fdf33dc05fae22b386fde
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-09-04 14:34:02 +0200

    functional programming template

---
+++ functional_programming/profile.py
@@ -0,0 +1,27 @@
+"""Write a function that prints a profile, given values."""
+
+def create_profile(given_name, *surnames, **details):
+    pass
+
+
+if __name__ == '__main__':
+    create_profile('Sam')
+    # Sam
+
+    create_profile('Sam', role='Instructor')
+    # Sam
+    # role: Instructor
+
+    create_profile('Martin', 'Luther', 'King', 'Jr', born=1929, died=1968)
+    # Martin Luther King Jr.
+    # born: 1929
+    # died: 1968
+
+    create_profile("Sebastian", "Thrun", cofounded="Udacity", experience="Stanford Professor")
+    # Sebastian Thrun
+    # cofounded: Udacity
+    # experience: Stanford Professor
+    # create_profile("Sam")
+    # create_profile("Martin", "Luther", "King", "Jr.", born=1929, died=1968)
+    # create_profile("Sebastian", "Thrun", cofounded="Udacity", experience="Stanford Professor")
+
