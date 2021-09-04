2d84a55fb9c5443130e8c31b1bff8f72a2a2b05a
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-09-04 15:45:25 +0200

    final function

--- functional_programming/profile.py
+++ functional_programming/profile.py
@@ -1,7 +1,14 @@
 """Write a function that prints a profile, given values."""
 
 def create_profile(given_name, *surnames, **details):
-    pass
+    if len(surnames)==0:
+        print(given_name)
+    else:
+        for surname in surnames:
+            given_name = given_name + " " + surname
+        print(given_name)
+    for key, value in details.items():
+        print(key, value, sep=": ")
 
 
 if __name__ == '__main__':
@@ -21,6 +28,10 @@ if __name__ == '__main__':
     # Sebastian Thrun
     # cofounded: Udacity
     # experience: Stanford Professor
+
+
+
+
     # create_profile("Sam")
     # create_profile("Martin", "Luther", "King", "Jr.", born=1929, died=1968)
     # create_profile("Sebastian", "Thrun", cofounded="Udacity", experience="Stanford Professor")
