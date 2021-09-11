d867e9fb97c8ba928cc7e5d93ae9a3687da26446
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-09-11 18:31:48 +0200

    fuzzer.py second solution inifnite stream itertools count

---
+++ functional_programming/fuzzer_secondsolution.py
@@ -0,0 +1,23 @@
+from itertools import count
+import random
+
+
+def random_list(size, start=0, stop=10):
+    return list(random.randrange(start, stop) for _ in range(size))
+
+
+def generate_cases():
+    return (
+        random_list(i) for i in count()
+    )
+
+if __name__ == '__main__':
+    for case in generate_cases():
+        if len(case) > 10:
+            break
+        print(case)
+
+    for i in count():
+        if i > 10:
+            break
+        print(i, "Teodor"*i)
