b1db3677746725aef0946e26e027f02e02adecae
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-09-13 10:30:58 +0200

    udacity solution edge cases

--- functional_programming/checker_udacity_solution.py
+++ functional_programming/checker_udacity_solution.py
@@ -37,24 +37,41 @@ def check_types(severity=1):
     return checker
 
 
-@check_types(severity=1)
+@check_types(severity=2)
 def foo(a: int, b: str) -> bool:
-    return b[a] == 'X'
+    res = b[a] == 'X'
+    if len(b)>4:
+        res = None
+    return res
 
 
 if __name__ == "__main__":
     # When used correctly, everything is great!
-    print(foo(3, 'ABCDE'))  # => False
+    print(foo(3, 'ABCD'))  # => False
     print(foo(1, 'WXYZ'))# => TRUE
 
     # But if the arguments are the wrong type, this decorator function will severely complain!
-    print(foo('WXYZ', 1))
+
     # Lots of information about an error
     # TypeError: Bad Argument! Received a=WXYZ, expecting object of type <class 'int'>
     #
     # Note that, since this is a function that produces decorators, even if we wanted to decorate it
     # with the default severity we'd need to call our function:
 
+    @check_types(severity=2)
+    def funkcja(a,b,c=2):
+        return a+b*c
+
+    print(funkcja("John", "Sally",c=5))
+
+
+    @check_types(severity=2)
+    def funkcja2(a:1,b,c=2):
+        return a+b*c
+
+    print(funkcja2("John", "Sally",c=5))
+
+    print(foo(1, 'WXYZdwfewa'))
     @check_types()  # <-- The function is still invoked.
     def foo(a: int, b: str) -> bool:
         return b[a] == 'X'
