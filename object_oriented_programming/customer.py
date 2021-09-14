81defd154683ae41436eacead24a964fd128471a
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-09-14 19:58:03 +0200

    property, static method, class method

--- object_oriented_programming/customer.py
+++ object_oriented_programming/customer.py
@@ -1,6 +1,26 @@
 class Customer:
-    pass
 
+    def __init__(self,firstname, lastname, tier="free", price=0):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.tier = tier
+        self.price = price
+
+    @property
+    def name(self):
+        return self.firstname + " " + self.lastname
+
+    def can_access(self,info):
+        if self.tier == 'premium':
+            return True
+        return info["tier"] == self.tier
+
+    @classmethod
+    def premium(cls,firstname, lastname):
+        return cls(firstname,lastname, tier='premium', price=10)
+
+    def bill_for(self,nb_moths):
+        return nb_moths*self.price
 
 if __name__ == '__main__':
     # This won't run until you implement the `Customer` class!
