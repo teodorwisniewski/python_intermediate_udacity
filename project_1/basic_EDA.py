506552cd03497832c9eefcbcfbf31fdd317c1131
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-09-18 19:52:18 +0200

    eda to answer several questions

--- project_1/basic_EDA.py
+++ project_1/basic_EDA.py
@@ -6,11 +6,13 @@ import pandas as pd
 
 filename = "neos.csv"
 df = pd.read_csv(filename)
-print(df)
-
 
 filename2 = "cad.json"
 with open("cad.json") as f:
     content = json.load(f)
 df2 = pd.DataFrame(content['data'], columns= content["fields"])
+masks = df2.cd.str.contains('2000-Jan-01')
 print(content)
+subset = df2[masks]
+
+info = subset[["des", "jd", "dist", "v_rel"]]
