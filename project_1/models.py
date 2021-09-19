93f458eabef9b31f52a94b0076727c6b844ac64e
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-09-19 10:58:22 +0200

    CloseApproach class and magic methods

--- project_1/models.py
+++ project_1/models.py
@@ -80,19 +80,16 @@ class CloseApproach:
     """
     # TODO: How can you, and should you, change the arguments to this constructor?
     # If you make changes, be sure to update the comments in this file.
-    def __init__(self, **info):
+    def __init__(self, _designation: str = '', time: str = None, distance: float = 0.0, velocity: float = 0.0,
+                 **info: Any):
         """Create a new `CloseApproach`.
 
         :param info: A dictionary of excess keyword arguments supplied to the constructor.
         """
-        # TODO: Assign information from the arguments passed to the constructor
-        # onto attributes named `_designation`, `time`, `distance`, and `velocity`.
-        # You should coerce these values to their appropriate data type and handle any edge cases.
-        # The `cd_to_datetime` function will be useful.
-        self._designation = ''
-        self.time = None  # TODO: Use the cd_to_datetime function for this attribute.
-        self.distance = 0.0
-        self.velocity = 0.0
+        self._designation = _designation
+        self.time = cd_to_datetime(time)
+        self.distance = distance
+        self.velocity = velocity
 
         # Create an attribute for the referenced NEO, originally None.
         self.neo = None
@@ -110,17 +107,14 @@ class CloseApproach:
         formatted string that can be used in human-readable representations and
         in serialization to CSV and JSON files.
         """
-        # TODO: Use this object's `.time` attribute and the `datetime_to_str` function to
-        # build a formatted representation of the approach time.
-        # TODO: Use self.designation and self.name to build a fullname for this object.
-        return ''
+        return datetime_to_str(self.time)
 
     def __str__(self):
         """Return `str(self)`."""
-        # TODO: Use this object's attributes to return a human-readable string representation.
         # The project instructions include one possibility. Peek at the __repr__
         # method for examples of advanced string formatting.
-        return f"A CloseApproach ..."
+        return f"CloseApproach(time={self.time_str}, distance={self.distance:.2f}, " \
+               f"velocity={self.velocity:.2f}, neo={self.neo})"
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
