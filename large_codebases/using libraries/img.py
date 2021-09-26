2fb0a1f03810e07adf2ba6fb0910500695b4ecb3
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-09-26 17:51:42 +0200

    img.py template

---
+++ large_codebases/using libraries/img.py
@@ -0,0 +1,16 @@
+def generate_postcard(in_path, out_path, message=None, crop=None, width=None):
+    """Create a Postcard With a Text Greeting
+
+    Arguments:
+        in_path {str} -- the file location for the input image.
+        out_path {str} -- the desired location for the output image.
+        crop {tuple} -- The crop rectangle, as a (left, upper, right, lower)-tuple. Default=None.
+        width {int} -- The pixel width value. Default=None.
+    Returns:
+        str -- the file path to the output image.
+    """
+
+    raise Exception('generate_postcard not implemented')
+
+if __name__=='__main__':
+    print(generate_postcard('./imgs/img.png'))
