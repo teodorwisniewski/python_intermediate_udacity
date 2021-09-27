cf26103b3a6eaac6932e409c14b54eddaedb5068
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-09-27 10:44:36 +0200

    importers

---
+++ large_codebases/using libraries/strategy_design_pattern/ImportEngine/ImportInterface.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from .Cat import Cat
+from typing import List
+
+
+class ImporterInterface(ABC):
+    allowed_exts = ['pdf', 'csv']
+
+    @classmethod
+    def can_ingest(cls, path: str) -> bool:
+        ext = path.split(".")[-1]
+        return ext in cls.allowed_exts
+
+    @classmethod
+    @abstractmethod
+    def parse(cls, path: str) -> List[Cat]:
+        pass
