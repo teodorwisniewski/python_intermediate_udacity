b817b8dcf2c925684813de99cebc7dfa6c2b01e6
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-10-09 10:41:27 +0200

    sys, name city Python

---
+++ large_codebases/python_in_systems/cli_sys.py
@@ -0,0 +1,10 @@
+# Import the sys library and replace the name variable with the appropriate argv index.
+# Add a second argument representing the city and extend the print to include this argument.
+# Try running python3 cli.py sam 'new york' and make sure you're getting the output: hello, sam from new york.
+# Notice the single quotes in the cli argument to prevent new and york from being passed as two different arguments.
+
+import sys
+
+name = sys.argv[1].lower().title()
+city = sys.argv[2].lower().title()
+print(f"hello, {name} from {city}.")
