0a36b3883b27be426c6331896602c57e74619ad0
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-10-09 11:53:55 +0200

    subprocess run

--- large_codebases/python_in_systems/arg_subprocess.py
+++ large_codebases/python_in_systems/arg_subprocess.py
@@ -1,10 +1,18 @@
+# import subprocess
+# import random
+
+# # p = subprocess.Popen(['emoj', 'dog'], stdout=subprocess.PIPE)
+# p = subprocess.run(["emoj", "dog"],stdout=subprocess.PIPE)
+
+# output, err = p.communicate()
+# p_status = p.wait()
+
+# emoji = output.decode('utf-8').split(' ')
+# print(random.choice(emoji))
+
+
 import subprocess
 import random
-
-p = subprocess.Popen(['emoj', 'dog'], stdout=subprocess.PIPE)
-
-output, err = p.communicate()
-p_status = p.wait()
-
-emoji = output.decode('utf-8').split(' ')
+p = subprocess.run(['emoj', 'dog'], stdout=subprocess.PIPE)
+emoji = p.stdout.decode('utf-8').split(' ')
 print(random.choice(emoji))
