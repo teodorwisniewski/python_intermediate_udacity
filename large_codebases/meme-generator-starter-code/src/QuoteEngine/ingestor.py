e4d4f644a981473491d561a4ca46ad16f67d110f
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-10-11 12:25:26 +0200

    CSVIngestor abc class

--- large_codebases/meme-generator-starter-code/src/QuoteEngine/ingestor.py
+++ large_codebases/meme-generator-starter-code/src/QuoteEngine/ingestor.py
@@ -6,15 +6,13 @@ from .quote_model import QuoteModel
 
 
 class IngestorInterface(ABC):
-    allowed_exts = ["csv"]
+    allowed_exts = []
 
     @classmethod
     def can_ingest(cls, path: str) -> bool:
         format = pathlib.Path(path).suffix[1:]
-        if format in cls.allowed_exts:
-            return True
-        else:
-            return False
+        format in cls.allowed_exts
+
 
     @classmethod
     @abstractmethod
