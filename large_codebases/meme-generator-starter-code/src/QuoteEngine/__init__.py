0363d98ff3b1727c9f595b7839fb0e49dbd8de24
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-10-17 12:19:36 +0200

    init QuoteEngine

---
+++ large_codebases/meme-generator-starter-code/src/QuoteEngine/__init__.py
@@ -0,0 +1,8 @@
+
+
+from .IngestorInterface import IngestorInterface
+from .Quote_Model import QuoteModel
+from .CSVingestor import CSVIngestor
+from .TXTingestor import TXTIngestor
+from .PDFingestor import PDFIngestor
+from .DOCXingestor import DOCXIngestor
