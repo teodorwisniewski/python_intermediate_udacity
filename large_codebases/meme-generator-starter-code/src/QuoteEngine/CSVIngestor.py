8a30f1a558a972b1acefeed0ede993e40eb0213f
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-10-19 20:44:43 +0200

    csv ingestor

--- large_codebases/meme-generator-starter-code/src/QuoteEngine/CSVIngestor.py
+++ large_codebases/meme-generator-starter-code/src/QuoteEngine/CSVIngestor.py
@@ -1,24 +1,28 @@
 from typing import List
-
+import pathlib
+import random
+import os
+import csv
 from .IngestorInterface import IngestorInterface
 from .QuoteModel import QuoteModel
 
 
-class TXTIngestor(IngestorInterface):
+class CSVIngestor(IngestorInterface):
 
-    allowed_exts = ["txt"]
+    allowed_exts = ["csv"]
 
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
         if cls.can_ingest(path):
             raise Exception(f"Extension is not allowed")
 
-        quotes = []
-
         with open(path, "r") as f:
-            for line in f:
-                body, author = line.split("-")
-                new_quote = QuoteModel(body.strip().strip('"'), author.strip())
+            quotes = []
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                body, author = row
+                new_quote = QuoteModel(body, author)
                 quotes.append(new_quote)
 
         return quotes
